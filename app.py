cf64f9e0fe540d83642da6c669be3246f73ad34c
Prathmesh Prabhakar Kate <kateprathamesh968@gmail.com>
2025-10-22 19:11:45 +0530

    Fixed import path for create_retrieval_chain

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings, HuggingFaceEndpoint
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
